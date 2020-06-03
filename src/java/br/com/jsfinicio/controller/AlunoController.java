5f2671233b0404f94e6367db7fb493b345a8cb2f
frankcpa <frankcpa84@gmai.com>
2020-06-03 15:04:34 -0400

    login, select, editar, excluir e elementos novos

--- src/java/br/com/jsfinicio/controller/AlunoController.java
+++ src/java/br/com/jsfinicio/controller/AlunoController.java
@@ -6,36 +6,81 @@
 package br.com.jsfinicio.controller;
 
 import br.com.jsfinicio.model.AlunoModel;
+import br.com.jsfinicio.model.EstadoModel;
 import br.com.jsfinicio.repository.AlunoRepository;
+import br.com.jsfinicio.repository.EstadoRepository;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author Frank
  */
 @ManagedBean
+@SessionScoped
 public class AlunoController {
-    
+
     private AlunoModel alunoModel;
+    private EstadoModel estadoModel;
+    private EstadoRepository estadoRepository;
     private AlunoRepository alunoRepository;
     private List<AlunoModel> listaDeAlunos;
-    
-    public AlunoController(){
+
+    public AlunoController() {
+        this.estadoRepository = new EstadoRepository();
         this.alunoModel = new AlunoModel();
+        this.estadoModel = new EstadoModel();
         this.alunoRepository = new AlunoRepository();
-        this.listaDeAlunos =  new ArrayList<>();
-    }
-    
-    public void salvar(){
-        this.alunoRepository.salvar(this.alunoModel);
+        this.listaDeAlunos = new ArrayList<>();
     }
 
-    public void buscar(){
+    public void salvar() {
+        try {
+            this.estadoModel = this.estadoRepository.buscarPorID(this.estadoModel.getIdEstado());
+            this.alunoModel.setEstado(this.estadoModel);
+            this.alunoRepository.salvar(this.alunoModel);
+            this.alunoModel = new AlunoModel();
+        } catch (Exception e) {
+        }
+    }
+
+    public void buscarTodos() {
         this.listaDeAlunos = this.alunoRepository.buscarTodos();
     }
-    
+
+    public void buscarPorNome() {
+        this.listaDeAlunos = this.alunoRepository.buscarPorNome(this.alunoModel.getNome());
+    }
+
+    public void excluirPorID(long idpessoa) {
+        this.alunoRepository.excluirPorID(idpessoa);
+    }
+
+    public String editarPorID(long idpessoa) throws IOException {
+        this.alunoModel = this.alunoRepository.buscarPorID(idpessoa);
+
+        return "editarAluno.xhtml?faces-redirect=true";
+    }
+
+    public EstadoRepository getEstadoRepository() {
+        return estadoRepository;
+    }
+
+    public void setEstadoRepository(EstadoRepository estadoRepository) {
+        this.estadoRepository = estadoRepository;
+    }
+
+    public EstadoModel getEstadoModel() {
+        return estadoModel;
+    }
+
+    public void setEstadoModel(EstadoModel estadoModel) {
+        this.estadoModel = estadoModel;
+    }
+
     public AlunoModel getAlunoModel() {
         return alunoModel;
     }
@@ -59,5 +104,5 @@ public class AlunoController {
     public void setListaDeAlunos(List<AlunoModel> listaDeAlunos) {
         this.listaDeAlunos = listaDeAlunos;
     }
-    
+
 }
--- src/java/br/com/jsfinicio/repository/AlunoRepository.java
+++ src/java/br/com/jsfinicio/repository/AlunoRepository.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class AlunoRepository extends Conexao{
     public void salvar(AlunoModel alunoModel){
         super.inicializa();
-        super.getSess().save(alunoModel);
+        super.getSess().saveOrUpdate(alunoModel);
         super.executar();
     }
     
@@ -28,4 +28,28 @@ public class AlunoRepository extends Conexao{
         super.executar();
         return listaDeAlunos;
     }
+    
+    public List<AlunoModel> buscarPorNome(String nome){
+        List<AlunoModel> listaDeAlunos = new ArrayList<>();
+        super.inicializa();
+        listaDeAlunos = super.getSess().createQuery("from AlunoModel where UPPER(nome) like '%" + nome.toUpperCase() + "%'").list();
+        super.executar();
+        return listaDeAlunos;
+    }
+   
+     public AlunoModel buscarPorID(long idPessoa){
+        AlunoModel aluno = new AlunoModel();
+        super.inicializa();
+        aluno = (AlunoModel) super.getSess().get(AlunoModel.class, idPessoa);
+        super.executar();
+        return aluno;
+    }
+    
+    public void excluirPorID(long idPessoa){
+        super.inicializa();
+        AlunoModel aluno = (AlunoModel) super.getSess().get(AlunoModel.class, idPessoa);
+        super.getSess().delete(aluno);
+        super.executar();
+    }
+    
 }
